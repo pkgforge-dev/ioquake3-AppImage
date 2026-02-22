40b6a827ff23a77ab51233300bab992233e828c6
Link4Electronics <link4electronics@gmail.com>
2026-02-22 02:52:31 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -22,4 +22,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --test ./dist/*.AppImage
+#quick-sharun --test ./dist/*.AppImage
