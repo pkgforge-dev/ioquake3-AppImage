717ba8c71937f4f45a4896f4d94c286b9229f30d
Link4Electronics <link4electronics@gmail.com>
2026-04-27 06:42:10 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,26 +6,42 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor       \
-    pipewire-audio \
-    pipewire-jack
+pacman -Syu --noconfirm cmake openal opusfile sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini opus-mini
 
-# Comment this out if you need an AUR package
-make-aur-package ioquake3-git
+echo "Building ioquake3..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/ioquake/ioq3"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./ioq3
+echo "$VERSION" > ~/version
+
 mkdir -p ./AppDir/bin
-mv -v /opt/quake3/* ./AppDir/bin
+wget -P /tmp http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3apoint-1.32b-3.x86.run
+chmod +x /tmp/linuxq3apoint-1.32b-3.x86.run
+/tmp/linuxq3apoint-1.32b-3.x86.run  --tar xf
+mv -v ./baseq3 ./missionpack ./AppDir/bin
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cmake -S ./ioq3 -B build -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SERVER=ON           \
+        -DBUILD_CLIENT=ON           \
+        -DBUILD_RENDERER_GL1=ON     \
+        -DBUILD_RENDERER_GL2=ON     \
+        -DBUILD_GAME_LIBRARIES=OFF  \
+        -DBUILD_GAME_QVMS=OFF       \
+        -DBUILD_STANDALONE=OFF      \
+        -DUSE_RENDERER_DLOPEN=ON    \
+        -DUSE_OPENAL=ON             \
+        -DUSE_OPENAL_DLOPEN=OFF     \
+        -DUSE_HTTP=ON               \
+        -DUSE_CODEC_VORBIS=ON       \
+        -DUSE_CODEC_OPUS=ON         \
+        -DUSE_VOIP=ON               \
+        -DUSE_MUMBLE=ON             \
+        -DUSE_FREETYPE=ON           \
+        -DUSE_INTERNAL_LIBS=OFF
+cmake --build build -j$(nproc)
+mv -v build/Release/* ./AppDir/bin
