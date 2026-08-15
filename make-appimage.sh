1efadc0d172bb4af67185439465ae3aa37ba6c41
Link4Electronics <link4electronics@gmail.com>
2026-08-15 07:02:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q ioquake3-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ioquake3-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -15,8 +15,6 @@ export DEPLOY_PULSE=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/ioquake3 ./AppDir/bin/*
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
-
-# Additional changes can be done in between here
 echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
