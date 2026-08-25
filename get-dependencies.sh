b61e55a4a8fe049519d571e0526d475f6344fecf
Link4Electronics <link4electronics@gmail.com>
2026-08-25 15:32:25 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,7 +31,7 @@ git clone "$REPO" ./ioq3
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-wget -P /tmp/ http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3apoint-1.32b-3.x86.run
+wget -P /tmp http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3apoint-1.32b-3.x86.run
 chmod +x /tmp/linuxq3apoint-1.32b-3.x86.run
 ./tmp/linuxq3apoint-1.32b-3.x86.run  --tar xf
 mv -v ./tmp/baseq3 ./tmp/missionpack ./AppDir/bin
