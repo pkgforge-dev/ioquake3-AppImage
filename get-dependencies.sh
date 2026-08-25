239e905a43ce3235ed2f6058a1a7c973fe398ef3
Link4Electronics <link4electronics@gmail.com>
2026-08-25 15:19:42 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,10 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake       \
+    openal      \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -29,4 +32,28 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 wget http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3apoint-1.32b-3.x86.run
+chmod +x ./linuxq3apoint-1.32b-3.x86.run
+tar -xfv ./linuxq3apoint-1.32b-3.x86.run
+
 cd ./ioq3
+cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SERVER=ON           \
+        -DBUILD_CLIENT=ON           \
+        -DBUILD_RENDERER_GL1=ON     \
+        -DBUILD_RENDERER_GL2=ON     \
+        -DBUILD_GAME_LIBRARIES=OFF  \
+        -DBUILD_GAME_QVMS=OFF       \
+        -DBUILD_STANDALONE=OFF      \
+        -DUSE_ARCHLESS_FILENAMES=ON \
+        -DUSE_RENDERER_DLOPEN=ON    \
+        -DUSE_OPENAL=ON             \
+        -DUSE_OPENAL_DLOPEN=OFF     \
+        -DUSE_HTTP=ON               \
+        -DUSE_CODEC_VORBIS=ON       \
+        -DUSE_CODEC_OPUS=ON         \
+        -DUSE_VOIP=ON               \
+        -DUSE_MUMBLE=ON             \
+        -DUSE_FREETYPE=ON           \
+        -DUSE_INTERNAL_LIBS=OFF
+
+cmake --build build
