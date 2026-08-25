1af029cae6ebf5710163f3b19c7096743aeddf07
Link4Electronics <link4electronics@gmail.com>
2026-08-25 15:39:10 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -33,10 +33,12 @@ echo "$VERSION" > ~/version
 mkdir -p ./AppDir/bin
 wget -P /tmp http://ftp.gwdg.de/pub/misc/ftp.idsoftware.com/idstuff/quake3/linux/linuxq3apoint-1.32b-3.x86.run
 chmod +x /tmp/linuxq3apoint-1.32b-3.x86.run
-/tmp/linuxq3apoint-1.32b-3.x86.run  --tar xf
-mv -v /tmp/baseq3 ./tmp/missionpack ./AppDir/bin
+ls
+cd /tmp
+./linuxq3apoint-1.32b-3.x86.run  --tar xf
+mv -v ./baseq3 ./missionpack ./AppDir/bin
 
-cd ./ioq3
+cd /ioq3
 cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SERVER=ON           \
         -DBUILD_CLIENT=ON           \
