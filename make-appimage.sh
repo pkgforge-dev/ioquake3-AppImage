3484eef000aa0e67a763c943ca75b02fc8db2759
Link4Electronics <link4electronics@gmail.com>
2026-08-29 20:58:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://github.com/ioquake/ioq3/raw/refs/heads/main/misc/quake3.svg
-export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 export DEPLOY_PULSE=1
 
 # Deploy dependencies
